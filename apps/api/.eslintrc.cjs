1e1c47647587e5c5b226d3236bf80979a2d6ec08
ARC Release Engineer <noreply@paperclip.ing>
2026-05-28 20:49:48 +0000

    fix(api): set eslint resolver projects for workspace imports

--- apps/api/.eslintrc.cjs
+++ apps/api/.eslintrc.cjs
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: ['@cipp-google/eslint-config/node'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['./tsconfig.json', '../../packages/*/tsconfig.json'],
+      },
+      node: true,
+    },
+  },
 };
